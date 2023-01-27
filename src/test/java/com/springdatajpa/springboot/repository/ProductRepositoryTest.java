00a14f3599c8407c8a89a609269d6ea2d345abdf
sajad <alisajadg191@gmail.com>
2023-01-27 16:48:26 +0000

    Initial commit

---
+++ src/test/java/com/springdatajpa/springboot/repository/ProductRepositoryTest.java
@@ -0,0 +1,58 @@
+package com.springdatajpa.springboot.repository;
+
+import com.springdatajpa.springboot.entity.Product;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ProductRepositoryTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    void saveMethod() {
+        // create product object
+        Product product = new Product();
+        product.setName("product 1");
+        product.setDescription("product 1 description");
+        product.setSku("100ABC");
+        product.setPrice(new BigDecimal(100));
+        product.setActive(true);
+        product.setImageUrl("product.png");
+
+        // save product
+        Product savedObject = productRepository.save(product);
+
+        // display product info
+        System.out.println(savedObject.getId());
+        System.out.println(savedObject.toString());
+    }
+
+    @Test
+    void updateUsingSaveMethod() {
+        // find or retrieve an entity by id
+        Long id = 1L;
+        Product product = productRepository.findById(id).get();
+
+        // update entity information
+        product.setName("updated product 1");
+        product.setDescription("updated product 1 desc");
+
+        // save updated entity
+        productRepository.save(product);
+    }
+
+    @Test
+    void findByIdMethod() {
+        Long id = 1L;
+
+        Product product = productRepository.findById(id).get();
+    }
+}
